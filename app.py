0d42aa0a5b39350069cc0cf74bbfac2b80b682bd
Manuel Chichi <manuelchichi96@gmail.com>
2021-10-19 19:59:54 -0300

    feature: Renombrado de rutas. Agregado proceso para obtener soluciones

--- app.py
+++ app.py
@@ -9,63 +9,125 @@ def hello_world():
     return "<p>API is UP.</p>"
 
 @app.route('/execution', methods=['POST'])
-def post_route():
+def post_execution():
     if request.method == 'POST':
         data = mongo.db.execution.insert(request.get_json())
         print('Data Received: "{data}"'.format(data=data))
         return "Request Processed.\n"
 
-@app.route('/get', methods=['GET'])
-def get_route():
+@app.route('/getSolutions/<int:execution_id>', methods=['GET'])
+def get_solutions(execution_id):
     if request.method == 'GET':
-
+        data = {
+                "id":1,
+                "prp_process_id":1,
+                "alternatives":
+                [
+                    {
+                        "alternative_id":1,
+                        "issues":
+                        [
+                            {
+                                "issue_id":1,
+                                "priority_id": 1
+                            },
+                            {
+                                "issue_id":2,
+                                "priority_id": 2
+                            },
+                            {
+                                "issue_id":3,
+                                "priority_id": 1
+                            },
+                            {
+                                "issue_id":4,
+                                "priority_id": 2
+                            },
+                            {
+                                "issue_id":5,
+                                "priority_id": 3
+                            }
+                        ]
+                    },
+                    {
+                        "alternative_id":2,
+                        "issues":
+                        [
+                            {
+                                "issue_id":1,
+                                "priority_id": 3
+                            },
+                            {
+                                "issue_id":2,
+                                "priority_id": 2
+                            },
+                            {
+                                "issue_id":3,
+                                "priority_id": 2
+                            },
+                            {
+                                "issue_id":4,
+                                "priority_id": 1
+                            },
+                            {
+                                "issue_id":5,
+                                "priority_id": 1
+                            }
+                        ]
+                    }
+                ]
+                     
+            }
+        return jsonify(data)
+@app.route('/getAlgorithms', methods=['GET'])
+def get_algorithms():
+    if request.method == 'GET':
         data = {   
-    "algorithms":
-    [
-        {
-            "id": 1,
-            "name": "NSGA-II",
-            "version": "1.0",
-            "parameters":
-            [
-                {"id": 1,
-                 "name": "Poblacion",
-                 "defaultValue": 100},
-                {"id": 2,
-                "name": "Generaciones",
-                 "defaultValue": 50},
-                {"id": 3,
-                 "name": "Probabilidad de mutacion",
-                 "defaultValue": 0.2},
-                {"id": 4,
-                 "name": "Ratio de Cruce",
-                 "defaultValue": 0.8}
-            ]
-        },
-        {
-            "id": 2,
-            "name": "MOPSO",
-            "version": "1.0",
-            "parameters":
-            [
-                {"id": 1,
-                 "name": "Poblacion",
-                 "defaultValue": 100},
-                {"id": 2,
-                 "name": "Generaciones",
-                 "defaultValue": 70},
-                {"id": 3,
-                 "name": "Probabilidad de mutacion",
-                 "defaultValue": 0.4},
-                {"id": 4,
-                 "name": "Ratio de Cruce",
-                 "defaultValue": 0.8},
-                {"id": 5,
-                 "name": "Ratio de deterioro",
-                 "defaultValue": 2}
-            ]
-        }
-    ]
-}
-        print('Data sent')
+                "algorithms":
+                [
+                    {
+                        "id": 1,
+                        "name": "NSGA-II",
+                        "version": "1.0",
+                        "parameters":
+                        [
+                            {"id": 1,
+                             "name": "Poblacion",
+                             "defaultValue": 100},
+                            {"id": 2,
+                            "name": "Generaciones",
+                             "defaultValue": 50},
+                            {"id": 3,
+                             "name": "Probabilidad de mutacion",
+                             "defaultValue": 0.2},
+                            {"id": 4,
+                             "name": "Ratio de Cruce",
+                             "defaultValue": 0.8}
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "name": "MOPSO",
+                        "version": "1.0",
+                        "parameters":
+                        [
+                            {"id": 1,
+                             "name": "Poblacion",
+                             "defaultValue": 100},
+                            {"id": 2,
+                             "name": "Generaciones",
+                             "defaultValue": 70},
+                            {"id": 3,
+                             "name": "Probabilidad de mutacion",
+                             "defaultValue": 0.4},
+                            {"id": 4,
+                             "name": "Ratio de Cruce",
+                             "defaultValue": 0.8},
+                            {"id": 5,
+                             "name": "Ratio de deterioro",
+                             "defaultValue": 2}
+                        ]
+                    }
+                ]
+            }
         return jsonify(data)
