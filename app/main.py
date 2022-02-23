36a7294008c3e4f4d4fb484d6a4c8d9e55dc82e0
Manuel <manuel.chichi@mikroways.net>
2022-02-23 16:16:54 -0300

    feature: Agregadas librerias faltantes. Correcciones a los modelos.

--- app/main.py
+++ app/main.py
@@ -1,34 +1,64 @@
 import os
 import numpy as np
+import asyncio
+from aiostream import stream
 
 from fastapi import FastAPI, status, BackgroundTasks
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import List
 import motor.motor_asyncio
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-db = client
+db = client['api']
 execution_collection = db.get_collection("executions_collection")
 
 
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
 # Models used for "execution"
 class CriteriaModel(BaseModel):
-    name: str
-    value: float
-
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    value: float = Field(...)
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Time",
+                "value": 15
+            }
+        }
 
 class IssueModel(BaseModel):
-    id: int
-    eval: [CriteriaModel]
-
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    issue_id: int = Field(...)
+    eval: List[CriteriaModel] = []
 
 class ExecutionModel(BaseModel):
-    prp_process_id: int
-    prp_execution_id: int
-    criteria: [CriteriaModel]
-    issues: [IssueModel]
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    prp_process_id: int = Field(...)
+    prp_execution_id: int = Field(...)
+    criteria: List[CriteriaModel] = []
+    issues: List[IssueModel] = []
 
 
 @app.get("/health")
@@ -267,7 +297,7 @@ def algorithms():
 # Obtain unfinished executions
 async def retrieve_executions():
     executions = []
-    async for execution in execution_collection.find():
+    async for execution in stream.enumerate(execution_collection.find()):
         if len(execution["solution"]) == 0:
             executions.append(execution)
     return executions
@@ -276,6 +306,6 @@ async def retrieve_executions():
 # On startup solve all unfinished executions
 @app.on_event("startup")
 async def startup_event():
-    pending_executions = retrieve_executions()
-    async for execution in pending_executions:
+    pending_executions = await retrieve_executions()
+    for execution in pending_executions:
         solve_execution(execution)
